7ca95cecf55da390808b5166c059052d5bbbe49d
utkusezen <utku.sezen7@gmail.com>
2025-08-26 22:38:51 +0200

    1.4 replace data load method with lazy loading
    load new tt100k + gtsdb dataset.
    
    replace data loading method with method that saves file paths to save memory.
    
    add dataset class that holds file paths and loads image and label data for direct use

--- backend/traffic_sign_detection_model.py
+++ backend/traffic_sign_detection_model.py
@@ -1,37 +1,80 @@
 import os
 import pandas as pd
 import numpy as np
+import torch
 from PIL import Image
+from torch.utils.data import Dataset
 from tqdm import tqdm
 
-TRAIN_DATA_PATH = "data/GTSDB/Train"
-TEST_DATA_PATH = "data/GTSDB/Test"
+TRAIN_DATA_PATH = "data/GTSDB_TT100k/Train"
+TEST_DATA_PATH = "data/GTSDB_TT100k/Test"
 
-def load_sign_detection_data(directory):
+def collect_sign_detection_path_data(directory):
     """
-    Loads image and label data from the given directory and returns them as numpy arrays
+    Collects image and label path data from the given directory. Does not open image data to save memory.
+    Images that do not have labels are assigned None as label path.
     :param directory: directory with subdirectories of images and labels
-    :return: X, y (data, labels)
+    :return: image path list and label path list
     """
     images_path = directory + "/images/"
     labels_path = directory + "/labels/"
-    images = []
-    labels = []
-    for image_file in tqdm(os.listdir(images_path)):
-        image = Image.open(images_path + image_file)
-        label_file = image_file.split(".")[0] + ".txt"
-        label = []
-        if not os.path.exists(labels_path + label_file):
-            label = [-1, -1, -1, -1]
+
+    path_data_images = []
+    path_data_labels = []
+    for image_path in tqdm(os.listdir(images_path)):
+        path_data_images.append(os.path.join(images_path, image_path))
+        label_path = os.path.join(labels_path, image_path.split(".")[0] + ".txt")
+        if os.path.exists(label_path):
+            path_data_labels.append(label_path)
         else:
-            with open(labels_path + label_file, "r") as f:
-                label = f.readline().split(" ")
-                label.pop(0)
-        images.append(image)
-        labels.append(list(map(float, label)))
-    return np.array(images), np.array(labels)
+            path_data_labels.append(None)
 
-train_x, train_y = load_sign_detection_data(TRAIN_DATA_PATH)
-test_x, test_y = load_sign_detection_data(TEST_DATA_PATH)
+    return path_data_images, path_data_labels
 
-print(train_x.shape, train_y.shape, test_x.shape, test_y.shape)
+def load_image_and_label_data(image_path, label_path):
+    """
+    Loads image and label data from given path and label data from given path.
+    Images that do not have labels are assigned a dummy label.
+    :param image_path: The path to the image
+    :param label_path: The path to the label
+    :return: image data and label data
+    """
+    image = Image.open(image_path).convert("RGB").copy()
+    bounding_boxes = []
+
+    if label_path:
+        with open(label_path, "r") as f:
+            lines = f.readlines()
+            bounding_boxes.append([list(map(float, l.strip().split())) for l in lines])
+    else:
+        bounding_boxes = [[-1, -1, -1, -1, -1]]
+
+    return image, bounding_boxes
+
+class TrafficSignDataset(Dataset):
+    """
+    A PyTorch Dataset class to simplify access and transformations on the data.
+    Uses paths to save memory and only loads data on access.
+    """
+    def __init__(self, image_paths, label_paths, transform=None):
+        self.image_paths = image_paths
+        self.label_paths = label_paths
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.image_paths)
+
+    def __getitem__(self, idx):
+        image_path = self.image_paths[idx]
+        label_path = self.label_paths[idx]
+
+        image, label = load_image_and_label_data(image_path, label_path)
+
+        if self.transform:
+            image = self.transform(image)
+
+        return image, label
+
+
+train_x, train_y = collect_sign_detection_path_data(TRAIN_DATA_PATH)
+test_x, test_y = collect_sign_detection_path_data(TEST_DATA_PATH)
