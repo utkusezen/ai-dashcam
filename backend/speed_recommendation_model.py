4aa3dce8be3ec89b78b35c2f5ffdb9c6e03379b4
utkusezen <utku.sezen7@gmail.com>
2025-10-04 17:19:03 +0200

    1.5 refactor method
    add description
    refactor var names
    add spacing

--- backend/speed_recommendation_model.py
+++ backend/speed_recommendation_model.py
@@ -9,18 +9,26 @@ TRAIN_LABEL_PATH = ""
 TEST_LABEL_PATH = ""
 
 def load_and_filter_training_data(path):
+    """
+    Loads data and extracts the two most important attributes: lanes and driveable_areas
+    :param path: the path to the json file
+    :return: lanes per images, driveable_areas per images
+    """
     lanes_per_image = []
     drivable_areas_per_image = []
     with open(path, "r") as f:
         data = json.load(f)
-        for d in data:
+        
+        for image_descr in data:
             lanes = []
             drivable_areas = []
-            for entry in d["labels"]:
-                if entry["category"] == "lane":
-                    lanes.append((entry["attributes"]["laneDirection"], entry["attributes"]["laneStyle"] ,entry["poly2d"][0]["vertices"]))
-                elif entry["category"] == "drivable area":
-                    drivable_areas.append(entry["poly2d"][0]["vertices"])
+
+            for label in image_descr["labels"]:
+                if label["category"] == "lane":
+                    lanes.append((label["attributes"]["laneDirection"], label["attributes"]["laneStyle"] ,label["poly2d"][0]["vertices"]))
+                elif label["category"] == "drivable area":
+                    drivable_areas.append(label["poly2d"][0]["vertices"])
+
             lanes_per_image.append(lanes)
             drivable_areas_per_image.append(drivable_areas)
     return lanes_per_image, drivable_areas_per_image
