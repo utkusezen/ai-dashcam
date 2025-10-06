69aa867b32eb6a7faf42dccc28dae7fce7f6659e
utkusezen <utku.sezen7@gmail.com>
2025-10-06 18:48:36 +0200

    1.5 refactor method and load labels
    refactor data loading method
    add manual labels for images
    load labels
    filter data for annotated only

--- backend/speed_recommendation_model.py
+++ backend/speed_recommendation_model.py
@@ -2,40 +2,53 @@ import json
 import pandas as pd
 from tqdm import tqdm
 
-TRAIN_DATA_PATH = "data/bdd100k/bdd100k_labels_release/bdd100k/labels/bdd100k_labels_images_train.json"
-TEST_DATA_PATH = "data/bdd100k/bdd100k_labels_release/bdd100k/labels/bdd100k_labels_images_train.json"
-
-TRAIN_LABEL_PATH = ""
-TEST_LABEL_PATH = ""
+DATA_PATH = "data/bdd100k/bdd100k_labels_release/bdd100k/labels/bdd100k_labels_images_train.json"
+LABELS_PATH = "data/bdd100k/manual_speed_recommendation_labels.csv"
 
 def load_and_filter_training_data(path):
     """
-    Loads data and extracts the two most important attributes: lanes and driveable_areas
+    Loads data and extracts the two most important attributes: image names, lanes and driveable_areas
     :param path: the path to the json file
-    :return: lanes per images, driveable_areas per images
+    :return: a pandas dataframe object containing image names, lanes and driveable areas
     """
     lanes_per_image = []
     drivable_areas_per_image = []
+    image_names = []
     with open(path, "r") as f:
         data = json.load(f)
-        
+
         for image_descr in data:
+            image_names.append(image_descr["name"])
             lanes = []
             drivable_areas = []
 
             for label in image_descr["labels"]:
                 if label["category"] == "lane":
-                    lanes.append((label["attributes"]["laneDirection"], label["attributes"]["laneStyle"] ,label["poly2d"][0]["vertices"]))
+                    lanes.append({
+                        "direction":    label["attributes"]["laneDirection"],
+                        "style":        label["attributes"]["laneStyle"] ,
+                        "vertices":     label["poly2d"][0]["vertices"]
+                    })
+
                 elif label["category"] == "drivable area":
                     drivable_areas.append(label["poly2d"][0]["vertices"])
 
             lanes_per_image.append(lanes)
             drivable_areas_per_image.append(drivable_areas)
-    return lanes_per_image, drivable_areas_per_image
 
+    filtered_data = pd.DataFrame()
+    filtered_data["name"] = image_names
+    filtered_data["lane"] = lanes_per_image
+    filtered_data["drivable_area"] = drivable_areas_per_image
+    return filtered_data
 
-lanes, drivable_areas = load_and_filter_training_data(TRAIN_DATA_PATH)
-print(f"lanes: {len(lanes)}, drivable areas: {len(drivable_areas)}")
+road_information_per_image = load_and_filter_training_data(DATA_PATH)
+
+labels = pd.read_csv(LABELS_PATH)
+labels = labels.drop(columns=["annotator", "lead_time", "updated_at", "created_at", "annotation_id", "id"])
+labels["image"] = [name[24:] for name in labels["image"]]
+annotated_road_information_per_image = road_information_per_image[road_information_per_image["name"].isin(labels["image"])]
+
 
 
 
