f3987c3799da56ebaf72f4205ef3fb611e844216
utkusezen <utku.sezen7@gmail.com>
2025-10-14 20:22:06 +0200

    1.5 restructure data
    add method that refactors label values
    sort labels and data by name

--- backend/speed_recommendation_model.py
+++ backend/speed_recommendation_model.py
@@ -1,5 +1,6 @@
 import os
 import cv2
+import numpy as np
 import pandas as pd
 import image_feature_extraction as ft_extr
 from tqdm import tqdm
@@ -8,6 +9,11 @@ DATA_PATH = "data/bdd100k/bdd100k/bdd100k/images/10k/train"
 LABELS_PATH = "data/bdd100k/manual_speed_recommendation_labels.csv"
 
 def load_data_and_extract_features(path):
+    """
+    Loads images and extracts features with the image_feature_extraction module.
+    :param path: the path to the images folder
+    :return: a dataframe containing the extracted features
+    """
     rows = []
     img_paths = os.listdir(path)
     for img_path in tqdm(img_paths):
@@ -34,11 +40,28 @@ def load_data_and_extract_features(path):
         })
     return pd.DataFrame(rows)
 
+def range_to_discrete_value(range_label:str):
+    """
+    Maps ranges of values to discrete values. Ranges are mapped to the maximum value in the range.
+    :param range_label: the range to map
+    :return: maximum value in the range
+    """
+    s = str(range_label).strip()
+    if s.startswith('<'):
+        return 10.0
+    if s.startswith('>'):
+       return 110.0
+    if '-' in s:
+        return float(s[3:])
+    return np.nan
 
 labels = pd.read_csv(LABELS_PATH)
 labels = labels.drop(columns=["annotator", "lead_time", "updated_at", "created_at", "annotation_id", "id"])
 labels["image"] = [name[24:] for name in labels["image"]]
+labels.sort_values("image", inplace=True)
+y = labels["choice"].apply(range_to_discrete_value).to_numpy()
 
 data = load_data_and_extract_features(DATA_PATH)
-print(data.shape)
-
+data.sort_values("image", inplace=True)
+data.drop(columns=["image"], inplace=True)
+X = data.to_numpy()
